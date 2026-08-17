3de5afb81b85382b0a62dc9e7f57e376fc34b343
dagerlot <marekkozykowski@gmail.com>
2026-08-17 22:23:33 +0200

    Add docker-compose

--- fitness_booking/settings.py
+++ fitness_booking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
